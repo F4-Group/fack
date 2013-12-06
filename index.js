eb544e445c87b034f76946485bbf95cf13ac33b1
Jazz <jazzzz@gmail.com>
2013-12-06 17:28:34 +0100

    Extracted fack

---
+++ index.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var os = require('os');
+
+var appName = require('./lib/appname')();
+process.title = appName;
+
+module.exports = {
+    sdc: require('./lib/sdc'),
+    optimist: require('./lib/optimist'),
+    express: require('./lib/express'),
+};
