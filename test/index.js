5aba17c2be723fd583a5f47f66237b2c44d789e3
Ludovic Perrine <lpe@f4-group.com>
2015-09-21 15:51:05 +0200

    Added test logs

--- test/index.js
+++ test/index.js
@@ -4,6 +4,12 @@ var logger = fack.logger;
 
 var app = fack.express();
 
+logger.info('Run with --debug to see debug logs');
+logger.debug('This log should appear with --debug option');
+logger.warn('Test warn log');
+logger.error('Test error log');
+logger.fatal('Test fatal log');
+
 app.get('/', function (req, res) {
     res.render('index');
 });
