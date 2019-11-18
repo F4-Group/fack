F4-Group/fack

commit b73838c0be8e2fe3e426b632527393d86351e52e

Fix mix of detected and configured bundles

lib/jsBundles.js
@@ -52,54 +52,46 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
         return p.replace(/\\/g, '/');
     }
 
-    function registerBrowserifyBundles(cb) {
+    function toCanonicalBundleConfig(bundleConf) {
         const defaultEntryDir = app.get("jsEntries")[0];
 
-        const manuallyConfiguredBundles = [];
-        let factoredBundleNames = [];
-        let bundles = _.map(options.bundles,
-            function (bundleConf) {
-                if (_.isString(bundleConf)) {
-                    manuallyConfiguredBundles.push(bundleConf);
-                    return {
-                        jsEntryDir: defaultEntryDir,
-                        name: nativePathSepToSlash(path.relative(defaultEntryDir, bundleConf)),
-                        path: bundleConf,
-                    };
-                } else {
-                    if (!bundleConf.jsEntryDir) {
-                        bundleConf.jsEntryDir = defaultEntryDir;
-                    }
-                    if (!bundleConf.path && !bundleConf.requires && !bundleConf.factorBundles) {
-                        if (!bundleConf.entryPoint) {
-                            throw new Error("bundleConf without entryPoint and path: " + JSON.stringify(bundleConf));
-                        } else {
-                            let {entryPoint} = bundleConf;
-                            if (!/\.js$/.test(entryPoint)) {
-                                entryPoint += ".js";
-                            }
-                            bundleConf.path = useNativePathSep(path.resolve(defaultEntryDir, entryPoint));
-                        }
-                    }
-                    if (!bundleConf.name) {
-                        if (!bundleConf.path) {
-                            throw new Error("bundleConf without path and name: " + JSON.stringify(bundleConf));
-                        } else {
-                            bundleConf.name = nativePathSepToSlash(path.relative(defaultEntryDir, bundleConf.path));
-                        }
-                    }
-                    manuallyConfiguredBundles.push(bundleConf.path);
-
-                    if (buildBundleOnFirstRequest) {
-                        delete bundleConf.factorBundles;
-                    }
-
-                    if (bundleConf.factorBundles) {
-                        factoredBundleNames = factoredBundleNames.concat(bundleConf.factorBundles);
-                    }
-                    return bundleConf;
+        if (_.isString(bundleConf)) {
+            bundleConf = {
+                entryPoint: bundleConf,
+            };
+        }
+        if (!bundleConf.path && !bundleConf.requires && !bundleConf.factorBundles) {
+            if (!bundleConf.entryPoint) {
+                throw new Error("bundleConf without entryPoint and path: " + JSON.stringify(bundleConf));
+            } else {
+                let {entryPoint} = bundleConf;
+                if (!/\.js$/.test(entryPoint)) {
+                    entryPoint += ".js";
                 }
-            });
+                bundleConf.path = useNativePathSep(path.resolve(defaultEntryDir, entryPoint));
+            }
+        }
+        delete bundleConf.entryPoint;
+        if (!bundleConf.name) {
+            if (!bundleConf.path) {
+                throw new Error("bundleConf without path and name: " + JSON.stringify(bundleConf));
+            } else {
+                bundleConf.name = nativePathSepToSlash(path.relative(defaultEntryDir, bundleConf.path));
+            }
+        }
+        if (buildBundleOnFirstRequest) {
+            delete bundleConf.factorBundles;
+        }
+        if (!bundleConf.name) {
+            bundleConf.name = useNativePathSep(path.relative(defaultEntryDir, bundleConf.path));
+        }
+        return bundleConf;
+    }
+
+    function registerBrowserifyBundles(cb) {
+        let bundles = _.map(options.bundles, toCanonicalBundleConfig);
+        const manuallyConfiguredBundleNames = _.map(bundles, 'name');
+        const factoredBundleNames = _.flatMap(bundles, conf => conf.factorBundles || []);
 
         _.each(app.get("jsEntries"), function (jsEntryDir) {
             let bundleConfiguration = {jsEntryDir};
@@ -107,18 +99,14 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
                 bundleConfiguration = _.clone(jsEntryDir);
             }
 
-            const jsEntriesPattern = path.join(bundleConfiguration.jsEntryDir, '**', '*.js');
-            const entries = glob.sync(jsEntriesPattern);
-            bundles = bundles.concat(_.map(
-                _.reject(_.map(entries, useNativePathSep), function (entry) {
-                    return _.includes(manuallyConfiguredBundles, entry);
-                }),
-                function (bundlePath) {
-                    return _.extend({
-                        name: nativePathSepToSlash(path.relative(bundleConfiguration.jsEntryDir, bundlePath)),
-                        path: bundlePath,
-                    }, bundleConfiguration);
-                }));
+            const detectedBundleNames = glob.sync('**/*.js', {
+                cwd: bundleConfiguration.jsEntryDir,
+            });
+            const detectedBundles = _(detectedBundleNames)
+                .reject(bundleName => _.includes(manuallyConfiguredBundleNames, bundleName))
+                .map(toCanonicalBundleConfig)
+                .value();
+            bundles = bundles.concat(detectedBundles);
         });
 
         let factoredBundles;
@@ -237,10 +225,7 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
         }
         const requires = bundleConfiguration.requires;
 
-        let name = bundleConfiguration.name;
-        if (!name) {
-            name = useNativePathSep(path.relative(bundleConfiguration.jsEntryDir, bundleConfiguration.path));
-        }
+        const name = bundleConfiguration.name;
 
         const contentType = 'text/javascript; charset=utf-8';
         const bundle = getBundle(bundleConfiguration);

commit cb8cefe9aec4c0f1d6f5b5f1b89cffffc72cfb1a

Use provided entryDir in js bundle
This fixes bundle injection from dependencies.
Bug introduced by b73838c0be8e2fe3e426b632527393d86351e52e

lib/jsBundles.js
@@ -52,9 +52,7 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
         return p.replace(/\\/g, '/');
     }
 
-    function toCanonicalBundleConfig(bundleConf) {
-        const defaultEntryDir = app.get("jsEntries")[0];
-
+    function toCanonicalBundleConfig(bundleConf, entryDir) {
         if (_.isString(bundleConf)) {
             bundleConf = {
                 entryPoint: bundleConf,
@@ -68,7 +66,7 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
                 if (!/\.js$/.test(entryPoint)) {
                     entryPoint += ".js";
                 }
-                bundleConf.path = useNativePathSep(path.resolve(defaultEntryDir, entryPoint));
+                bundleConf.path = useNativePathSep(path.resolve(entryDir, entryPoint));
             }
         }
         delete bundleConf.entryPoint;
@@ -76,20 +74,21 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
             if (!bundleConf.path) {
                 throw new Error("bundleConf without path and name: " + JSON.stringify(bundleConf));
             } else {
-                bundleConf.name = nativePathSepToSlash(path.relative(defaultEntryDir, bundleConf.path));
+                bundleConf.name = nativePathSepToSlash(path.relative(entryDir, bundleConf.path));
             }
         }
         if (buildBundleOnFirstRequest) {
             delete bundleConf.factorBundles;
         }
         if (!bundleConf.name) {
-            bundleConf.name = useNativePathSep(path.relative(defaultEntryDir, bundleConf.path));
+            bundleConf.name = useNativePathSep(path.relative(entryDir, bundleConf.path));
         }
         return bundleConf;
     }
 
     function registerBrowserifyBundles(cb) {
-        let bundles = _.map(options.bundles, toCanonicalBundleConfig);
+        const defaultEntryDir = app.get("jsEntries")[0];
+        let bundles = _.map(options.bundles, _.partial(toCanonicalBundleConfig, _, defaultEntryDir));
         const manuallyConfiguredBundleNames = _.map(bundles, 'name');
         const factoredBundleNames = _.flatMap(bundles, conf => conf.factorBundles || []);
 
@@ -104,7 +103,7 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
             });
             const detectedBundles = _(detectedBundleNames)
                 .reject(bundleName => _.includes(manuallyConfiguredBundleNames, bundleName))
-                .map(toCanonicalBundleConfig)
+                .map(_.partial(toCanonicalBundleConfig, _, bundleConfiguration.jsEntryDir))
                 .value();
             bundles = bundles.concat(detectedBundles);
         });