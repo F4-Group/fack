9f23b279f83687e1901a15698d279509ca44a8ca
Charly Koza <cka@f4-group.com>
2020-05-25 10:07:27 +0200

    Add uniqueProcessName to be able to uniquely identify a dokku process
    When starting processes on many hosts, with dokku scale, we sometimes
    need a unique name for locks and synchronization.

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = {
     appName: require('./lib/appname')(),
     hostname: require('./lib/hostname'),
     i18n: require('i18next'),
+    uniqueProcessName: require('./lib/uniqueProcessName'),
 };
 
 uncaughtException.bind();
