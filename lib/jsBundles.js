F4-Group/fack

commit 59391fbcc2c4caf0ab798613cb74ed8237157642

feat: allow to pass Pug compiler options
The `pugify` option can now be an object containing the Pug compiler
options.

lib/jsBundles.js
@@ -201,11 +201,12 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
             fullPaths: options.fullPaths,
         });
         const transforms = _.clone(bundleConfiguration.transforms);
-        if (bundleConfiguration.pugify) {
-            transforms.push(pugify.pug({
+        const mostSpecificPugifyOption = _.has(bundleConfiguration, pugify) ? bundleConfiguration.pugify : options.pugify;
+        if (mostSpecificPugifyOption !== false) {
+            transforms.push(pugify.pug(_.extend({
                 pretty: false,
                 compileDebug: pugDebug,
-            }));
+            }, options.pugify, bundleConfiguration.pugify)));
         }
         if (options.babelify !== false) {
             const babelifyOptions = _.extend({

commit cd25f8c402674618ccb86356fe61c6703098987d

fix: fix ignored 'pugify' option in bundle configuration
If was broken since 59391fbcc2c4caf0ab798613cb74ed8237157642.

lib/jsBundles.js
@@ -201,7 +201,7 @@ function jsBundles(app, options, cacheforeverMiddleware, i18nOptions) {
             fullPaths: options.fullPaths,
         });
         const transforms = _.clone(bundleConfiguration.transforms);
-        const mostSpecificPugifyOption = _.has(bundleConfiguration, pugify) ? bundleConfiguration.pugify : options.pugify;
+        const mostSpecificPugifyOption = _.has(bundleConfiguration, 'pugify') ? bundleConfiguration.pugify : options.pugify;
         if (mostSpecificPugifyOption !== false) {
             transforms.push(pugify.pug(_.extend({
                 pretty: false,